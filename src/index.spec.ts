8265ee3ebde0cfac61f977bcac85925fc6870a8e
RozennJ <rozenn.jezegou@ensta-bretagne.org>
2022-11-22 12:50:46 +0100

    test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,10 +1,5 @@
-/*describe('typeScript test suite', () => {
-  it('should return "Hello world!"', () => {
-    expect.assertions(1);
-    expect(helloWorld()).toBe('Hello world!');
+describe('creating server', () => {
+  it('should return a http server', () => {
+    const server = module.require("./index.ts").
   });
-});*/
-test('server is listening ', async () => {
-  const data = module.require('./index').requesting();
-  expect(data).toBe('Server listening on port 3030');
 });
